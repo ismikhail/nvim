b79ba424b3c76456425135ab420512f5a03fde08
Mikhail Smirnov <msmirnov.y@gmail.com>
2021-11-05 22:00:10 -0700

    update nvmtree config

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -1,9 +1,35 @@
+vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.nvim_tree_icons = {
+    git = {
+    unstaged = "",
+    staged = "S",
+    unmerged = "",
+    renamed = "➜",
+    deleted = "",
+    untracked = "U",
+    ignored = "◌",
+  },
+}
+
 require'nvim-tree'.setup {
-  auto_close = true,
+  auto_close = false,
   view = {
     allow_resize = true,
     side = 'right',
     width = 70
+  },
+  diagnostics = {
+    enable = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  ignore = { ".git", "node_modules", ".cache", ".next", ".vscode" },
+  update_focused_file = {
+    enable = true
   }
 }
 
