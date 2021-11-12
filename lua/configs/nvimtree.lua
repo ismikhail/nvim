6ba643585982562b4419c6f3a4ef2dc903d1e400
Mikhail Smirnov <msmirnov.y@gmail.com>
2021-11-11 23:02:17 -0800

    update nvimtree config

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -1,18 +1,40 @@
+-- vim.g.nvim_tree_show_icons = {
+--   git = 1,
+--   folders = 0,
+--   files = 1,
+--   folder_arrows = 1
+-- }
+
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_icons = {
+  icons = {
+    default = "",
+    symlink = "",
     git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
+      unstaged = "",
+      staged = "S",
+      unmerged = "",
+      renamed = "➜",
+      deleted = "",
+      untracked = "U",
+      ignored = "◌",
+    },
+    folder = {
+      default = "",
+      open = "",
+      empty = "",
+      empty_open = "",
+      -- default = "",
+      -- open = "",
+      -- empty = "",
+      -- empty_open = "",
+      symlink = "",
+    },
   },
 }
 
 require'nvim-tree'.setup {
-  auto_close = false,
+  auto_close = true,
   view = {
     allow_resize = true,
     side = 'right',
