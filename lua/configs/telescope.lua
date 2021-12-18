244f214f4832ab462937fb6f712f14cb67139c01
Mikhail Smirnov <msmirnov.y@gmail.com>
2021-12-17 16:18:27 -0800

    use fzf with telescope

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,5 +1,16 @@
+local telescope = require('telescope')
 local actions = require('telescope.actions')
-require('telescope').setup {
+
+telescope.setup {
+  extensions = {
+     fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  },
   defaults = {
     layout_config = {
       width = 0.75,
@@ -46,3 +57,5 @@ require('telescope').setup {
     }
   }
 }
+
+telescope.load_extension('fzf')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -42,6 +42,7 @@ packer.startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use {
     'folke/which-key.nvim',
     config = function()
